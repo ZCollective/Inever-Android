6efb6b44fd4a35eac7ca8c0d38eb769065f7cc62
Andreas Knipl <andreas.knipl@zcollective.de>
2020-01-18 12:44:40 +0100

    Refactor Settings-Activity

--- app/src/main/java/de/zigldrum/ihnn/views/Settings.java
+++ app/src/main/java/de/zigldrum/ihnn/views/Settings.java
@@ -25,25 +25,32 @@ public class Settings extends AppCompatActivity {
 
     private AppState state;
 
+    private Switch nsfwSwitch;
+    private Switch nsfwOnlySwitch;
+    private Switch autoUpdateSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         Log.i(LOG_TAG, "Settings was created!");
         Log.d(LOG_TAG, "Message: " + getIntent().getStringExtra("msg"));
+
         state = AppState.loadState(getFilesDir());
-        Switch nsfwSwtich = findViewById(R.id.settings_nsfw);
-        nsfwSwtich.setChecked(state.getEnableNSFW());
-        Switch autoUpdateSwitch = findViewById(R.id.settings_auto_update);
-        autoUpdateSwitch.setChecked(state.getEnableAutoUpdates());
-        Switch nsfwOnlySwitch = findViewById(R.id.settings_nsfw_only);
+
+        nsfwSwitch = findViewById(R.id.settings_nsfw);
+        nsfwOnlySwitch = findViewById(R.id.settings_nsfw_only);
+        autoUpdateSwitch = findViewById(R.id.settings_auto_update);
+
+        nsfwSwitch.setChecked(state.getEnableNSFW());
         nsfwOnlySwitch.setChecked(state.isOnlyNSFW());
+        autoUpdateSwitch.setChecked(state.getEnableAutoUpdates());
     }
 
     public void toggleNSFW(View v) {
-        Switch nsfwSwitch = findViewById(R.id.settings_nsfw);
         boolean newValue = nsfwSwitch.isChecked();
-        if (newValue != state.getEnableNSFW()){
+        if (newValue != state.getEnableNSFW()) {
             state.setEnableNSFW(newValue);
             stateUpdated = true;
         }
@@ -51,31 +58,29 @@ public class Settings extends AppCompatActivity {
     }
 
     public void toggleAutoUpdate(View v) {
-        Switch autoUpdateSwitch = findViewById(R.id.settings_auto_update);
         boolean newValue = autoUpdateSwitch.isChecked();
-        if (newValue != state.getEnableAutoUpdates()){
+        if (newValue != state.getEnableAutoUpdates()) {
             state.setEnableAutoUpdates(newValue);
             stateUpdated = true;
         }
-        Log.i(LOG_TAG, "Autoupdates are " + (autoUpdateSwitch.isChecked() ? "enabled" : "disabled"));
+        Log.i(LOG_TAG, "Auto Updates are " + (autoUpdateSwitch.isChecked() ? "enabled" : "disabled"));
     }
 
-    public void toggleNSFWOnlyMode(View v){
-        Switch nsfwOnlySwitch = findViewById(R.id.settings_nsfw_only);
+    public void toggleNSFWOnlyMode(View v) {
         boolean newValue = nsfwOnlySwitch.isChecked();
-        if (newValue != state.isOnlyNSFW()){
+        if (newValue != state.isOnlyNSFW()) {
             state.setOnlyNSFW(newValue);
             stateUpdated = true;
         }
         Log.i(LOG_TAG, "NSFW Only mode is " + (nsfwOnlySwitch.isChecked() ? "enabled" : "disabled"));
     }
 
-    public void updateNow (View v) {
+    public void updateNow(View v) {
         updateTriggered = true;
         this.finish();
     }
 
-    public void goBack (View v) {
+    public void goBack(View v) {
         Log.i(LOG_TAG, "K, thx bai!");
         this.finish();
     }
@@ -84,7 +89,9 @@ public class Settings extends AppCompatActivity {
     public void finish() {
         if (stateUpdated) state.saveState(getFilesDir());
         Intent data = new Intent();
-        int resultCode = updateTriggered && stateUpdated ? STATE_AND_UPDATE : (updateTriggered ? UPDATENOW : (stateUpdated ? STATE_CHANGED : DEFAULT));
+        int stateChanged = stateUpdated ? STATE_CHANGED : DEFAULT;
+        int updated = updateTriggered ? UPDATENOW : stateChanged;
+        int resultCode = updateTriggered && stateUpdated ? STATE_AND_UPDATE : updated;
         setResult(resultCode, data);
         super.finish();
     }
