15258174d685e96361b825d3bf9ab7c0b17c556e
Andreas Knipl <andreas.knipl@zcollective.de>
2020-02-11 16:16:17 +0100

    Optimize Game-Cycle
    Every time the Game-Cycle was done, all questions got discarded & added again.
    This is a very ineffective way of doing this, as in practical terms, we were
    just starting the list all over again. Also the app stability suffered if
    the "Next"-Button was pressed in a fast succession. These two issues can be
    tackled with one solution.
    
    - Questions for the Game-Cycle are only added once
    - On repeat the List-Iterator will be acquired once again, to start anew
    - Every iteration for the game-logic runs in a sequential background thread
    - We will be failing in onPostCreate if no question is available

--- app/src/main/java/de/zigldrum/ihnn/activities/Game.java
+++ app/src/main/java/de/zigldrum/ihnn/activities/Game.java
@@ -1,5 +1,6 @@
 package de.zigldrum.ihnn.activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,9 +20,8 @@ import de.zigldrum.ihnn.R;
 import de.zigldrum.ihnn.networking.objects.ContentPack;
 import de.zigldrum.ihnn.networking.objects.Question;
 import de.zigldrum.ihnn.utils.AppState;
-import de.zigldrum.ihnn.utils.Utils;
-
-import static de.zigldrum.ihnn.utils.Constants.AgeRestrictions.NSFW_BORDER;
+import de.zigldrum.ihnn.utils.Constants.AgeRestrictions;
+import de.zigldrum.ihnn.utils.Constants.GameResults;
 
 public class Game extends AppCompatActivity {
 
@@ -26,53 +29,67 @@ public class Game extends AppCompatActivity {
 
     private final AppState state = AppState.getInstance(null);  // null allowed -> should already be instantiated;
 
-    private List<Question> questions;
+    private volatile Iterator<Question> questionIterator;
+    private volatile List<Question> questions = new ArrayList<>();
+    private volatile boolean shownRepeatOption = false;
 
-    private boolean shownRepeatOption = false;
+    private TextView questionDisplay;
+    private Button nextButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_game);
 
-        Log.i(LOG_TAG, "Game was created!");
+        Log.i(LOG_TAG, "Running Game::onCreate()");
         Log.d(LOG_TAG, "Message: " + getIntent().getStringExtra("msg"));
+
+        questionDisplay = findViewById(R.id.question_text);
+        nextButton = findViewById(R.id.btn_game_next);
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        questions = getAndFilterQuestions();
-        if (questions.isEmpty()) {
-            Utils.showLongToast(getApplicationContext(), getResources().getString(R.string.info_no_questions_available));
-            finish();
-        }
+        Log.i(LOG_TAG, "Running Game::onPostCreate()");
+
+        addFilteredQuestions();
+
+        boolean isEmpty = questions.isEmpty();
+        setResult(isEmpty ? GameResults.GAME_QUESTIONS_EMPTY : GameResults.GAME_DEFAULT);
+        if (isEmpty) finish();
+
+        questionIterator = questions.iterator();
     }
 
     public void showNext(View v) {
-        Log.d(LOG_TAG, "Getting next Question!");
+        // This increases our ability to handle fast clicking, otherwise we might crash
+        AsyncTask.SERIAL_EXECUTOR.execute(() -> {
+            Log.d(LOG_TAG, "Getting next Question!");
 
-        TextView questionDisplay = findViewById(R.id.question_text);
-
-        if (questions.isEmpty()) {
-            Button nextButton = findViewById(R.id.btn_game_next);
-
-            if (!shownRepeatOption) {
-                questionDisplay.setText(R.string.game_no_questions_left);
-                nextButton.setText(R.string.btn_game_repeat);
-                shownRepeatOption = true;
-            } else {
-                questions = getAndFilterQuestions();
-                nextButton.setText(R.string.btn_next);
+            if (shownRepeatOption) {
+                Log.d(LOG_TAG, "Starting a new round!");
+                runOnUiThread(() -> nextButton.setText(R.string.btn_next));
+                Collections.shuffle(questions);
+                questionIterator = questions.iterator();
                 shownRepeatOption = false;
-                Question question = questions.remove((int) (Math.random() * questions.size()));
-                questionDisplay.setText(question.getString());
             }
-        } else {
-            Question question = questions.remove((int) (Math.random() * questions.size()));
-            questionDisplay.setText(question.getString());
-        }
+
+            if (questionIterator.hasNext()) {
+                Question nextQuestion = questionIterator.next();
+                runOnUiThread(() -> questionDisplay.setText(nextQuestion.getString()));
+            } else {
+                Log.d(LOG_TAG, "Last question in selection!");
+
+                runOnUiThread(() -> {
+                    questionDisplay.setText(R.string.game_no_questions_left);
+                    nextButton.setText(R.string.btn_game_repeat);
+                });
+                shownRepeatOption = true;
+            }
+        });
     }
 
     public void goBack(View view) {
@@ -80,26 +97,26 @@ public class Game extends AppCompatActivity {
         finish();
     }
 
-    private List<Question> getAndFilterQuestions() {
+    private void addFilteredQuestions() {
+        questions.clear();
+
         List<ContentPack> enabledPacks = state.getPacks()
-                .stream()
-                .filter(p -> !state.getDisabledPacks().contains(p.getId()))
-                .collect(Collectors.toList());
+                                              .stream()
+                                              .filter(p -> !state.getDisabledPacks().contains(p.getId()))
+                                              .collect(Collectors.toList());
 
         if (state.isOnlyNSFW()) {
             Log.d(LOG_TAG, "Ignoring other age settings. NSFWOnly mode overrides!");
             List<Integer> nsfwPackIDs = enabledPacks
                     .stream()
-                    .filter(p -> p.getMinAge() >= NSFW_BORDER)
+                    .filter(p -> p.getMinAge() >= AgeRestrictions.NSFW_BORDER)
                     .map(ContentPack::getId)
                     .collect(Collectors.toList());
 
-            return state
-                    .getQuestions()
-                    .parallelStream()
-                    .filter(q -> nsfwPackIDs.contains(q.getPackid()))
-                    .collect(Collectors.toList());
-
+            state.getQuestions()
+                 .stream()
+                 .filter(q -> nsfwPackIDs.contains(q.getPackid()))
+                 .forEach(questions::add);
         } else if (state.getEnableNSFW()) {
             Log.d(LOG_TAG, "NSFW Mode enabled.");
             List<Integer> enabledPackIDs = enabledPacks
@@ -107,26 +124,24 @@ public class Game extends AppCompatActivity {
                     .map(ContentPack::getId)
                     .collect(Collectors.toList());
 
-            return state
-                    .getQuestions()
-                    .parallelStream()
-                    .filter(q -> enabledPackIDs.contains(q.getPackid()))
-                    .collect(Collectors.toList());
-
+            state.getQuestions()
+                 .stream()
+                 .filter(q -> enabledPackIDs.contains(q.getPackid()))
+                 .forEach(questions::add);
         } else {
             Log.d(LOG_TAG, "NON-NSFW Mode enabled.");
             List<Integer> nonNsfwPackIDs = enabledPacks
                     .stream()
-                    .filter(p -> p.getMinAge() < NSFW_BORDER)
+                    .filter(p -> p.getMinAge() < AgeRestrictions.NSFW_BORDER)
                     .map(ContentPack::getId)
                     .collect(Collectors.toList());
 
-            return state
-                    .getQuestions()
-                    .parallelStream()
-                    .filter(q -> nonNsfwPackIDs.contains(q.getPackid()))
-                    .collect(Collectors.toList());
+            state.getQuestions()
+                 .stream()
+                 .filter(q -> nonNsfwPackIDs.contains(q.getPackid()))
+                 .forEach(questions::add);
         }
-    }
 
+        Collections.shuffle(questions);
+    }
 }
